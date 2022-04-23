c480709f4768cde7aa738c0c642e82bfb5dfa125
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-04-23 15:12:30 +0400

    Сверстал главную страну

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,11 +8,31 @@ const NavbarItem = styled.div`
 position:relative;
 `
 const Logo = styled.a`
-font-family: 'Roboto';
 font-style: normal;
 font-weight: 500;
 font-size: 18px;
 line-height: 21px;
+color:#3260A1;
+`
+const AddPostBtn = styled.a`
+font-style: normal; 
+font-weight: 300;
+font-size: 14px;
+line-height: 16px;
+color: #FFFFFF;
+background: #67BFFF;
+box-shadow: 0px 10px 25px rgba(148, 174, 213, 0.15);
+border-radius: 10px;
+position: absolute;
+width: 139px;
+height: 25px;
+left:50%;
+top:25px;
+display:flex;
+justify-content:center;
+align-items:center;
+transform:translate(-50%);
+cursor:pointer;
 `
 
 export default function Navbar () {
@@ -23,6 +43,9 @@ export default function Navbar () {
 <Logo>
     NEXT | BLOG
 </Logo>
+<AddPostBtn>
+   Добавить статью 
+</AddPostBtn>
 </NavbarItem>
 </div>
         </Nav>
--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,74 @@ import Head from "next/head";
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
 
-const Title = styled.h1`
-  color: red;
+const Wrapper = styled.div`
+  background: #eef5ff;
+  min-height: 100vh;
+  width: 100%;
+`;
+const PostWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  column-gap: 30px;
+`;
+const Post = styled.a`
+  width: 350px;
+  height: 270px;
+  border-radius: 15px;
+  margin-top: 50px;
+  position: relative;
+  cursor: pointer;
+  background: url("${(props) => props.bgImage}") center / cover no-repeat;
+  :hover {
+    transform:scale(1.1);
+    transition: 3s all ease;
+  }
+`;
+const PostTitle = styled.div`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 21px;
+  padding: 15px 20px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: white;
+  border-radius: 0px 0px 15px 15px;
 `;
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <Head>
         <title>Create Next App</title>
       </Head>
-      <Navbar/>
+      <Navbar />
+      <Wrapper>
+        <div className="container">
+          <PostWrapper>
+            <Post bgImage={"./static/images/1.png"}>
+              <PostTitle>Мальдивы. Рай или пафос ?</PostTitle>
+            </Post>
+            <Post bgImage={"./static/images/2.png"}>
+              <PostTitle>Италия. Остров Капри. Обзор. </PostTitle>
+            </Post>
+            <Post bgImage={"./static/images/3.png"}>
+              <PostTitle>США. Сан-Франциско,  дорого ?</PostTitle>
+            </Post>
+            <Post bgImage={"./static/images/4.png"}>
+              <PostTitle>Канада. Пейзажи вблизи Онтарио.</PostTitle>
+            </Post>
+            <Post bgImage={"./static/images/5.png"}>
+              <PostTitle>Швейцария.  Красота природы.</PostTitle>
+            </Post>
+            <Post bgImage={"./static/images/6.png"}>
+              <PostTitle>Альпы.  Покори вершину с нами !</PostTitle>
+            </Post>
+          </PostWrapper>
+        </div>
+      </Wrapper>
     </div>
   );
 }
